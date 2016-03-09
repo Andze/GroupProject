3b129acbfe5c96d5e30753ee035e999da1ebfe2c
Dale Taylor <dimthegamer115@gmail.com>
2016-03-09 22:46:01 +0000

    some message goes here

---
+++ GroupProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour 
+{
+    int dealersFirstCard = -1;
+
+    public CardStack player;
+    public CardStack dealer;
+    public CardStack deck;
+
+    public Button hitButton;
+    public Button stickButton;
+    
+    /*
+     * Cards dealt to each player
+     * First player hits/sticks/bust
+     * Dealer's turn; must have minimum of 17 score hand
+     * Dealers cards; first card is hidden, subsequent cards are facing
+     */
+
+    #region Public methods
+
+    public void Hit()
+    {
+        player.Push(deck.Pop());
+        if (player.HandValue() > 21)
+        {
+            // TODO: The player is bust
+            hitButton.interactable = false;
+            stickButton.interactable = false;
+        }
+    }
+
+    public void Stick()
+    {
+        hitButton.interactable = false;
+        stickButton.interactable = false;
+        StartCoroutine(DealersTurn());
+    }
+
+    #endregion
+
+    #region Unity messages
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    #endregion
+
+    void StartGame()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            player.Push(deck.Pop());
+            HitDealer();
+        }
+    }
+
+    void HitDealer()
+    {
+        int card = deck.Pop();
+
+        if (dealersFirstCard < 0)
+        {
+            dealersFirstCard = card;
+        }
+
+        dealer.Push(card);
+        if (dealer.CardCount >= 2)
+        {
+            CardStackView view = dealer.GetComponent<CardStackView>();
+            view.Toggle(card, true);
+        }
+    }
+
+    IEnumerator DealersTurn()
+    {
+        while (dealer.HandValue() < 17)
+        {
+            HitDealer();
+            yield return new WaitForSeconds(1f);
+        }
+
+        CardStackView view = dealer.GetComponent<CardStackView>();
+        view.Toggle(dealersFirstCard, true);
+    }
+}
