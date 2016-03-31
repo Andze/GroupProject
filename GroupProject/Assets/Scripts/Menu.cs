d89845c2e9a1eab867243e10c940775c357ae422
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-03-31 18:40:19 +0100

    Added Menu

---
+++ GroupProject/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas Main;
+    public Canvas Options;
+
+    void Awake()
+    {
+        Options.enabled = false;
+    }
+    public void OptionsOn()
+    {
+        Options.enabled = true;
+        Main.enabled = false;
+    }
+    public void ReturnsOn()
+    {
+        Options.enabled = false;
+        Main.enabled = true;
+    }
+    public void LoadOn()
+    {
+        Application.LoadLevel (1);
+    }
+}
