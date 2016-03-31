2bb256518a194d9c715c7b849ec1e63ebbf6ce15
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-03-31 19:39:03 +0100

    Revert "bug fixing and space on table for ai added"
    This reverts commit 969a06b8214858818fd45baf08d1ded86a4d4036.

---
+++ GroupProject/Assets/Scripts/Menu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas Main;
+    public Canvas Options;
+    public Canvas Rules;
+
+
+    void Awake()
+    {
+        Options.enabled = false;
+        Rules.enabled = false;
+    }
+    public void OptionsOn()
+    {
+        Options.enabled = true;
+        Main.enabled = false;
+        Rules.enabled = false;
+    }
+    public void ReturnsOn()
+    {
+        Options.enabled = false;
+        Main.enabled = true;
+        Rules.enabled = false;
+    }
+
+    public void RulesOn()
+    {
+        Rules.enabled = true;
+        Main.enabled = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void LoadOn()
+    {
+        Application.LoadLevel (1);
+    }
+}
