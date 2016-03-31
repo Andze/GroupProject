a8c4e3c83ff1c5308409bd852135557975af813c
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-03-31 19:01:11 +0100

    Added Full menu (Play,Rules,Options,Exit)

--- GroupProject/Assets/Scripts/Menu.cs
+++ GroupProject/Assets/Scripts/Menu.cs
@@ -6,20 +6,36 @@ public class Menu : MonoBehaviour
 {
     public Canvas Main;
     public Canvas Options;
+    public Canvas Rules;
+
 
     void Awake()
     {
         Options.enabled = false;
+        Rules.enabled = false;
     }
     public void OptionsOn()
     {
         Options.enabled = true;
         Main.enabled = false;
+        Rules.enabled = false;
     }
     public void ReturnsOn()
     {
         Options.enabled = false;
         Main.enabled = true;
+        Rules.enabled = false;
+    }
+
+    public void RulesOn()
+    {
+        Rules.enabled = true;
+        Main.enabled = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
     public void LoadOn()
     {
