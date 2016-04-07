16fa0384bd54e4ed94a77bed315118cb5d6a2e8b
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-04-07 02:15:24 +0100

    Added Background Images and changed layout of buttons as well as Loading bar bug Fixed

--- GroupProject/Assets/Scripts/LoadingBar.cs
+++ GroupProject/Assets/Scripts/LoadingBar.cs
@@ -7,6 +7,7 @@ public class LoadingBar : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Image image = GetComponent<Image>();
+        image.fillAmount = 0f;
     }
 	
 	// Update is called once per frame
