9c5090b82c3bd1d41076009c2e00915c9ed6eaa6
Dale Taylor <dimthegamer115@gmail.com>
2016-04-07 18:07:40 +0100

    added AI Script, finalized win states, bug fixing

--- GroupProject/Assets/Scripts/GameController.cs
+++ GroupProject/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class GameController : MonoBehaviour 
 {
     int dealersFirstCard = -1;
@@ -14,7 +15,11 @@ public class GameController : MonoBehaviour
     public InputField Input;
     public Text CurrentBet;
     public Text Player1Liqudity;
-    public Text Player2Liquidity;
+    public Text Player2Cash;
+    public int Player1Con = 5;
+    public int Player2Con = 5;
+    public int DealerCon = 5;
+    public int hitValue = 0;
 
     public Button hitButton;
     public Button stickButton;
@@ -38,15 +43,18 @@ public class GameController : MonoBehaviour
             // TODO: The player is bust
             hitButton.interactable = false;
             stickButton.interactable = false;
+            Input.interactable = false;
+            StartCoroutine(AITurn());
         }
+
     }
 
     public void Stick()
     {
+        Input.interactable = false;
         hitButton.interactable = false;
         stickButton.interactable = false;
-//        StartCoroutine(AiAlgoritham());       line below must be deleted and added in when the algoritham has finished it's turn 
-        StartCoroutine(DealersTurn());
+        StartCoroutine(AITurn());       //line below must be deleted and added in when the algoritham has finished it's turn 
     }
     public void PlayAgain()
     {
@@ -60,7 +68,8 @@ public class GameController : MonoBehaviour
         deck.CreateDeck();
         hitButton.interactable = true;
         stickButton.interactable = true;
-
+        Input.interactable = true;
+        WinnerText.text = " ";
         StartGame();
     }
     public void Updatebet()
@@ -81,6 +90,8 @@ public class GameController : MonoBehaviour
 
             CurrentBet.text = y.ToString();
             Player1Liqudity.text = PlayerLiquid.ToString();
+            Input.text = "Thanks for the money";
+            AIBet();
         }
         else
         {
@@ -138,46 +149,221 @@ public class GameController : MonoBehaviour
         view.Toggle(dealersFirstCard, true);
         view.ShowCards();
         yield return new WaitForSeconds(1f);
-
-        while (dealer.HandValue() < 17)
-        {
-            HitDealer();
-            yield return new WaitForSeconds(1f);
-        }
-        if (player.HandValue() > 21 && Player2.HandValue() > 21 || dealer.HandValue() >= player.HandValue() && dealer.HandValue() >= Player2.HandValue() && dealer.HandValue() <= 21)
+        if (player.HandValue() > 21 && Player2.HandValue() > 21)
         {
             WinnerText.text = "Dealer Wins!";
             CurrentBet.text = "0";
-
+            DealerCon++;
+            Player1Con--;
+            Player2Con--;
         }
-        else if (dealer.HandValue() > 21 && player.HandValue() <= 21 && Player2.HandValue() <= 21)
+        else
         {
-            WinnerText.text = "both players win, pot is split!";
-            int.TryParse(CurrentBet.text, out y);
-            int.TryParse(Player1Liqudity.text, out z);
-            int.TryParse(Player2Liquidity.text, out x);
-            y = y / 2;
-            z += y;
-            x += y;
-            Player1Liqudity.text = z.ToString();
-            Player2Liquidity.text = x.ToString();
-        }
-        else if (player.HandValue() >= Player2.HandValue() && player.HandValue() <= 21)
-        {
-            WinnerText.text = "player 1 Wins!";
-            int.TryParse(CurrentBet.text, out y);
-            int.TryParse(Player1Liqudity.text, out z);
-            z += y;
-            Player1Liqudity.text = z.ToString();
-        }
-        else if (Player2.HandValue() >= player.HandValue() && Player2.HandValue() <= 21)
-        {
-            WinnerText.text = "player 2 Wins!";
-            int.TryParse(CurrentBet.text, out y);
-            int.TryParse(Player2Liquidity.text, out x);
-            x += y;
-            Player2Liquidity.text = x.ToString();
+            while (dealer.HandValue() < 17)
+            {
+                HitDealer();
+                yield return new WaitForSeconds(1f);
+            }
+            if (player.HandValue() > 21 && Player2.HandValue() > 21 && dealer.HandValue() <= 21 || dealer.HandValue() >= player.HandValue() && dealer.HandValue() >= Player2.HandValue() && dealer.HandValue() <= 21)
+            {
+                WinnerText.text = "Dealer Wins!";
+                CurrentBet.text = "0";
+                DealerCon++;
+                Player1Con--;
+                Player2Con--;
+            }
+            else if (dealer.HandValue() > 21 && player.HandValue() <= 21 && Player2.HandValue() <= 21)
+            {
+                WinnerText.text = "both players win, pot is split!";
+                int.TryParse(CurrentBet.text, out y);
+                int.TryParse(Player1Liqudity.text, out z);
+                int.TryParse(Player2Cash.text, out x);
+                y = y / 2;
+                z += y;
+                x += y;
+                Player1Liqudity.text = z.ToString();
+                Player2Cash.text = x.ToString();
+                CurrentBet.text = "0";
+                Player1Con++;
+                Player2Con++;
+                DealerCon--;
+            }
+            else if (player.HandValue() > Player2.HandValue() && player.HandValue() <= 21 && Player2.HandValue() <= 21 || player.HandValue() < Player2.HandValue() && player.HandValue() <= 21 && Player2.HandValue() > 21)
+            {
+                WinnerText.text = "player 1 Wins!";
+                int.TryParse(CurrentBet.text, out y);
+                int.TryParse(Player1Liqudity.text, out z);
+                z += y;
+                Player1Liqudity.text = z.ToString();
+                CurrentBet.text = "0";
+                Player1Con++;
+                Player2Con--;
+                DealerCon--;
+            }
+            else if (Player2.HandValue() >= player.HandValue() && Player2.HandValue() <= 21 && player.HandValue() <= 21 || Player2.HandValue() < player.HandValue() && Player2.HandValue() <= 21 && player.HandValue() > 21)
+            {
+                WinnerText.text = "player 2 Wins!";
+                int.TryParse(CurrentBet.text, out y);
+                int.TryParse(Player2Cash.text, out x);
+                x += y;
+                Player2Cash.text = x.ToString();
+                CurrentBet.text = "0";
+                Player2Con++;
+                Player1Con--;
+                DealerCon--;
+            }
+            else
+            {
+                WinnerText.text = "Dealer Wins";
+                CurrentBet.text = "0";
+                DealerCon++;
+                Player1Con--;
+                Player2Con--;
+            }
         }
         PlayAgainButton.interactable = true;
+    }
+    IEnumerator AITurn()
+    {
+
+        if (Player2.HandValue() > 20)
+        {
+            StopAllCoroutines();
+            StartCoroutine(DealersTurn());
+           
+        }
+        yield return new WaitForSeconds(1f);
+        double L = GameLogic();
+        L *= 10;
+        double C = Player2Con;
+        double G = Greed();
+        double R = Random.value;
+        double T = 0.35;
+        double Q = 0;
+         Q =( (L * 0.4) + (0.1 * C) + (0.4 * G) + R)/10;
+
+        Debug.Log(Q);
+        if (Q >= T)
+        {
+            Player2.Push(deck.Pop());
+            StartCoroutine(AITurn());
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(DealersTurn());
+            yield return new WaitForSeconds(30f);
+        }
+
+    }
+    int GameLogic()
+    {
+        int y = 0;
+        int x = 0;
+
+        hitButton.interactable = false;
+        stickButton.interactable = false;
+         x = Player2.HandValue();
+        y = dealer.HandValue();
+
+        if (x <= 4 && x >= 11)
+        {
+
+            return 1;
+        }
+        else if (x > 11 && y >= 7)
+        {
+            
+            return 1;
+        }
+        else if (x >= 12 && x <= 16 && y >= 7 && y <= 11)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+    void AIBet()
+    {
+        int StartingBet = 0;
+        int Playercash = 0;
+        int PlayerBet = 0;
+        int.TryParse(Player2Cash.text, out StartingBet);
+        int temp = StartingBet;
+        StartingBet = StartingBet / 5;
+
+        int.TryParse(Player1Liqudity.text, out Playercash);
+        int.TryParse(CurrentBet.text, out PlayerBet);
+        PlayerBet = PlayerBet / 2;
+
+        int BettingDifference = PlayerBet - StartingBet;
+
+
+        if (BettingDifference == 0)
+        {
+            StartingBet += 5;
+        }
+        else if (BettingDifference > StartingBet)
+        {
+            StartingBet *= 2;
+            if (StartingBet > temp)
+            {
+                StartingBet -= BettingDifference;
+
+            }
+        }
+        else if ((StartingBet * 2) < PlayerBet)
+            StartingBet *= 2;
+
+
+
+        PlayerBet *= 2;
+        temp -= StartingBet;
+        Player2Cash.text = temp.ToString();
+
+        PlayerBet += (StartingBet * 2);
+
+        CurrentBet.text = PlayerBet.ToString();
+
+
+    }
+
+    double Greed()
+    {
+        double x, y;
+        double.TryParse(CurrentBet.text, out y);
+        double.TryParse(Player2Cash.text, out x);
+        //400,000 is the highest possible aamount of money in the game as such it's our 100% value
+        x = x / 4000;   y = y / 4000;
+
+        if (x > y * 2)
+        {
+            return 10;
+        }
+        if (x < 10)
+            return 0;
+        else if (x > 10 && x < 20)
+            return 1;
+        else if (x > 20 && x < 30)
+            return 2;
+        else if (x > 30 && x < 40)
+            return 3;
+        else if (x > 40 && x < 50)
+            return 4;
+        else if (x > 50 && x < 60)
+            return 5;
+        else if (x > 60 && x < 70)
+            return 6;
+        else if (x > 70 && x < 80)
+            return 7;
+        else if (x > 80 && x < 90)
+            return 8;
+        else if (x > 90 && x < 99)
+            return 9;
+        else
+            return 10;
     }
 }
